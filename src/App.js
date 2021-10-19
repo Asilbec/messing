a3cde43d425fb6c99ed51243520ecee6e2ee4fd9
Asilbec <asilbekomonkulov2003@gmail.com>
2021-10-19 01:17:05 -0400

    just messing around

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="troll">
+        <a href='https://youtu.be/Ixrje2rXLMA?t=76' id='nice'><h1>Derek</h1></a>
+        <a href='https://youtu.be/m0dg6PzVPgU?t=92' id='nice'><h1>Tphill</h1></a>
+        <a href='https://youtu.be/TkDCL4mgg1U?t=29' id='nice'><h1>Joe</h1></a>
+        <a href='https://youtu.be/Xu3lGKOJlbs?t=16' id='nice'><h1>Josie</h1></a>
+
+
+      </div>
     </div>
   );
 }
